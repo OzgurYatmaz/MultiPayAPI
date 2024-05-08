625d31c3a554bc1e820167080e96fd6540a4fd28
OzgurYatmaz <ozguryatmaz@gmail.com>
2024-05-08 12:44:21 +0300

    Mock service tool is updated for multiple service integration

--- payment-service-sample/src/main/java/com/multipay/configuration/PaymentServiceConfig.java
+++ payment-service-sample/src/main/java/com/multipay/configuration/PaymentServiceConfig.java
@@ -8,21 +8,32 @@ import org.springframework.stereotype.Component;
 public class PaymentServiceConfig {
 
 	private String paymentserviceurl;
+	private String paymentservice2url;
+
 	private int timeout;
-	
+
 	public String getPaymentserviceurl() {
 		return paymentserviceurl;
 	}
+
 	public void setPaymentserviceurl(String paymentserviceurl) {
 		this.paymentserviceurl = paymentserviceurl;
 	}
+
+	public String getPaymentservice2url() {
+		return paymentservice2url;
+	}
+
+	public void setPaymentservice2url(String paymentservice2url) {
+		this.paymentservice2url = paymentservice2url;
+	}
+
 	public int getTimeout() {
 		return timeout;
 	}
+
 	public void setTimeout(int timeout) {
 		this.timeout = timeout;
 	}
-	 
-	
 
 }
